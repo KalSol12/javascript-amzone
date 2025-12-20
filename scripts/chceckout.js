4b6cec978e9e4d79c0e5c22c9c7beff74b86777a
DESKTOP-SN0UNKK\Hp <tenkirkaleab@gmail.com>
2025-12-20 15:13:11 +0300

    Delete product from the cart when clicking delete btn

--- scripts/chceckout.js
+++ scripts/chceckout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils.js";
 
@@ -44,7 +44,7 @@ cart.forEach((cartItem)=>{
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingItem.id}">
                     Delete
                   </span>
                 </div>
@@ -103,3 +103,12 @@ cart.forEach((cartItem)=>{
 orderSummery.innerHTML = orderSummeryHTML;
 
   
+
+document.querySelectorAll('.js-delete-link').forEach((link)=>{
+  link.addEventListener('click',()=>{
+    const productId=link.dataset.productId;
+    removeFromCart(productId)
+  })
+
+
+});
